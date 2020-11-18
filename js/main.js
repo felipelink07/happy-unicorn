64848a3654271d973ac9df4d6de1210505a5f7a9
Felipe Felix <felipfelixsantos07@gmail.com>
2020-11-17 22:36:00 -0300

    Mudança na estrutura total do projeto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const toggleButton = document.getElementById('toggle-button');
+const navBarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+
+  navBarLinks.classList.toggle('show');
+
+})
